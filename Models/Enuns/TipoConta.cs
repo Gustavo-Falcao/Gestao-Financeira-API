b538e9219461a5d3b5ee8ca9c3cb7b6038b52d37
Gustavo <duartegusfalcao@gmail.com>
2026-04-20 20:34:14 -0300

    Adicioanando pastas e entidades do projeto.

---
+++ Models/Enuns/TipoConta.cs
@@ -0,0 +1,9 @@
+namespace GestaoFinanceira.Models.Enuns
+{
+    public enum TipoConta
+    {
+        Corrente = 1,
+        Poupanca = 2,
+        Carteira = 3
+    }
+}
---
+++ Models/Enuns/TipoMovimentacao.cs
@@ -0,0 +1,8 @@
+namespace GestaoFinanceira.Models.Enuns
+{
+    public enum TipoMovimentacao
+    {
+        Receita = 1,
+        Despesa = 2
+    }
+}
