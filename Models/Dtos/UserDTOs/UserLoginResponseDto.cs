48b4fcc422bc31a552a04fffbb603bc99cf12da6
Gustavo <duartegusfalcao@gmail.com>
2026-05-27 20:26:16 -0300

    Finalizado authentication controller e service.

--- Models/Dtos/UserDTOs/UserLoginResponseDto.cs
+++ Models/Dtos/UserDTOs/UserLoginResponseDto.cs
@@ -3,7 +3,5 @@ namespace Gestao_Financeira.Models.Dtos.UserDTOs
     public class UserLoginResponseDto
     {
         public string Token { get; set; } = string.Empty;
-        public string Nome { get; set; } = string.Empty;
-        public string Email { get; set; } = string.Empty;
     }
 }
